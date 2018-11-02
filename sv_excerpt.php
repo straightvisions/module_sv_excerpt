8921f0c6c8d41219db85951faa17eff5eefa7050
Matthias Reuter <matthias-reuter@straightvisions.com>
2018-11-02 09:09:42 +0100

    SUB - ADD - sv_excerpt

--- sv_excerpt.php
+++ sv_excerpt.php
@@ -9,11 +9,15 @@
 	 * @since			1.0
 	 * @license			See license.txt or https://straightvisions.com
 	 */
-	class sv_abstract extends init{
+	class sv_excerpt extends init{
 		const section_title							= 'Abstract';
 		static $scripts_loaded						= false;
 
 		public function __construct($path,$url){
+			$this->set_section_title('Excerpt');
+			$this->set_section_desc(__('Adjust Settings', $this->get_module_name()));
+			$this->set_section_type('settings');
+			
 			$this->path								= $path;
 			$this->url								= $url;
 			$this->name								= get_class($this);
@@ -22,11 +26,12 @@
 			add_action('init', array($this, 'init'));
 		}
 		public function admin_init(){
-			$this->get_root()->add_section($this, 'settings');
+			$this->get_root()->add_section($this);
 			$this->load_settings();
 		}
 		public function init(){
 			add_filter('excerpt_length', array($this, 'excerpt_length'));
+			add_filter('excerpt_more', array($this, 'excerpt_more'));
 
 			if(!is_admin()){
 				$this->load_settings();
@@ -34,17 +39,24 @@
 		}
 		public function load_settings(){
 			$this->s['length'] = static::$settings->create($this)
-				->set_section_group($this->get_module_name())
-				->set_section_name('Abstract Settings')
-				->set_section_description(__('Adjust Settings', $this->get_module_name()))
 				->set_ID('length')
-				->set_title('Abstract Length')
+				->set_title('Excerpt Length')
 				->set_description(__('Maximum of chars allowed in displaying excerpts. Default is 80.', $this->get_module_name()))
 				->set_placeholder('80')
 				->load_type('number');
+			
+			$this->s['more'] = static::$settings->create($this)
+				->set_ID('more')
+				->set_title('Excerpt Read More Text')
+				->set_description(__('Read More Text for Excerpts, default ...', $this->get_module_name()))
+				->set_placeholder('...')
+				->load_type('text');
 		}
 		public function excerpt_length($length){
 			return $this->s['length']->run_type()->get_data() ? $this->s['length']->run_type()->get_data() : 80;
+		}
+		public function excerpt_more(){
+			return $this->s['more']->run_type()->get_data() ? $this->s['more']->run_type()->get_data() : '...';
 		}
 	}
 ?>
