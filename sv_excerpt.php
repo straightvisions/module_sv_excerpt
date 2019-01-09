3fa621715c3e8b2e4ce7574f8d02c1c5cd92c532
Adrian Chudzynski <adrian-chudzynsk@straightvisions.com>
2019-01-09 21:40:02 +0100

    Refactored

--- sv_excerpt.php
+++ sv_excerpt.php
@@ -9,18 +9,27 @@ namespace sv_100;
  * @since			1.0
  * @license			See license.txt or https://straightvisions.com
  */
+
 class sv_excerpt extends init {
 	public function __construct() {
 	
 	}
+
 	public function init() {
+		// Module Info
+		$this->set_module_title( 'SV Excerpt' );
+		$this->set_module_desc( __( 'This module manages how excerpts will be displayed.', $this->get_module_name() ) );
+
+		// Section Info
 		$this->set_section_title( 'Excerpt' );
 		$this->set_section_desc( __( 'Adjust Settings', $this->get_module_name() ) );
 		$this->set_section_type( 'settings' );
 		$this->get_root()->add_section( $this );
+
+		// Loads Settings
 		$this->load_settings();
-		
-		add_action( 'admin_init', array( $this, 'admin_init' ) );
+
+		// Action Hooks
 		add_filter( 'excerpt_length', array( $this, 'excerpt_length' ) );
 		add_filter( 'excerpt_more', array( $this, 'excerpt_more' ) );
 	}
