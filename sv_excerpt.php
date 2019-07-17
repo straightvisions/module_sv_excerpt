245e1ce2a3bef520dd50a50d43270e2662fa3239
Adrian Chudzynski <adrian-chudzynsk@straightvisions.com>
2019-07-17 20:13:55 +0200

    Updated get_settings method

--- sv_excerpt.php
+++ sv_excerpt.php
@@ -13,48 +13,42 @@
 	
 	class sv_excerpt extends init {
 		public function init() {
-			// Module Info
-			$this->set_module_title( 'SV Excerpt' );
-			$this->set_module_desc( __( 'This module gives the ability to define how excerpts will be displayed.', 'sv100' ) );
-	
-			// Section Info
-			$this->set_section_title( __( 'Excerpt', 'sv100' ) );
-			$this->set_section_desc( __( 'Adjust Settings', 'sv100' ) );
-			$this->set_section_type( 'settings' );
-			$this->get_root()->add_section( $this );
-	
-			// Loads Settings
-			$this->load_settings();
+			$this->set_module_title( 'SV Excerpt' )
+				 ->set_module_desc( __( 'This module gives the ability to define how excerpts will be displayed.', 'sv100' ) )
+				 ->load_settings()
+				 ->set_section_title( __( 'Excerpt', 'sv100' ) )
+				 ->set_section_desc( __( 'Adjust Settings', 'sv100' ) )
+				 ->set_section_type( 'settings' )
+				 ->get_root()
+				 ->add_section( $this );
 	
 			// Action Hooks
 			add_filter( 'excerpt_length', array( $this, 'excerpt_length' ) );
 			add_filter( 'excerpt_more', array( $this, 'excerpt_more' ) );
 		}
 	
-		public function load_settings() {
-			$this->s['length'] =
-				$this->get_setting()
-					 ->set_ID( 'length' )
-					 ->set_title( __( 'Excerpt length', 'sv100' ) )
-					 ->set_description( __( 'Maximum number of words allowed in displaying excerpts.', 'sv100' ) )
-					 ->set_placeholder( '30' )
-					 ->set_default_value( 30 )
-					 ->load_type( 'number' );
-	
-			$this->s['more'] =
-				$this->get_setting()
-					 ->set_ID( 'more' )
-					 ->set_title( __( 'Text to show at the end of the excerpt', 'sv100' ) )
-					 ->set_placeholder( '...' )
-					 ->set_default_value( '...' )
-					 ->load_type( 'text' );
+		protected function load_settings(): sv_excerpt {
+			$this->get_setting( 'length' )
+				 ->set_title( __( 'Excerpt length', 'sv100' ) )
+				 ->set_description( __( 'Maximum number of words allowed in displaying excerpts.', 'sv100' ) )
+				 ->set_placeholder( '30' )
+				 ->set_default_value( 30 )
+				 ->load_type( 'number' );
+			
+			$this->get_setting( 'more' )
+				 ->set_title( __( 'Text to show at the end of the excerpt', 'sv100' ) )
+				 ->set_placeholder( '...' )
+				 ->set_default_value( '...' )
+				 ->load_type( 'text' );
+			
+			return $this;
 		}
 	
-		public function excerpt_length( $length ) {
-			return $this->get_setting( 'length' )->run_type()->get_data() ? $this->get_setting( 'length' )->run_type()->get_data() : 80;
+		public function excerpt_length( int $length ): int {
+			return $this->get_setting( 'length' )->run_type()->get_data() ? $this->get_setting( 'length' )->run_type()->get_data() : 30;
 		}
 	
-		public function excerpt_more() {
+		public function excerpt_more(): string {
 			return $this->get_setting( 'more' )->run_type()->get_data() ? $this->get_setting( 'more' )->run_type()->get_data() : '...';
 		}
 	}
